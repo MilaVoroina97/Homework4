fddd1cb8950df90aa48c012bd441c111fe7b1ea4
mila <milavoronina1997@mail.ru>
2022-09-01 20:16:34 +0500

    task11

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,9 +5,11 @@
 //2, 4 -> 16
 
 Console.WriteLine("Enter first integer number");
-int a = Convert.ToInt32(Console.ReadLine());
+int x = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter second integer number");
-int b = Convert.ToInt32(Console.ReadLine());
-int x = a*Math.Pow(b);
-Console.WriteLine(x);
+int power = Convert.ToInt32(Console.ReadLine());
+for (power = 0; power <= power; power++)
+{
+    Console.WriteLine($"{x}^{power} = {Math.Pow(x, power)}");
+}
 
