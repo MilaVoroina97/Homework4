2547e9778d21a8fcfe6228cd81b176695d053887
mila <milavoronina1997@mail.ru>
2022-09-01 20:16:41 +0500

    task12

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и 
+//положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+int[]array = new int[12];
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-9,10);
+        if (i!=array.Length-1) Console.Write($"{array[i]}, ");
+        else Console.WriteLine($"{array[i]}]");
+    }
+int sum_p = 0;
+int sum_n = 0;
+string s = "";
+foreach (int item in array)
+{
+    if (item > 0) 
+    {
+        sum_p +=item;
+        s+= $"{item} + ";
+    }
+    else sum_n += item;
+
+}
+Console.WriteLine($"Сумма положительных чисел: {sum_p}");
+Console.WriteLine($"Сумма отрицательных чисел: {sum_n}");
